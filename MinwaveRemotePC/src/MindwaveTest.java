6ea0a74a5cedff31d49cf68e2bca1ad4edb9cc3d
nirajprakash <nirprjj@gmail.com>
2014-06-08 17:27:35 +0530

    added mindwave library :

---
+++ MinwaveRemotePC/src/MindwaveTest.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.TooManyListenersException;
+
+import javax.comm.CommPortIdentifier;
+import javax.comm.PortInUseException;
+import javax.comm.SerialPort;
+import javax.comm.SerialPortEvent;
+import javax.comm.SerialPortEventListener;
+import javax.comm.UnsupportedCommOperationException;
+
+
+
+
+public class MindwaveTest {
+	
+	
+	static CommPortIdentifier portId;
+    static Enumeration portList;
+
+    
+    //InputStream inputStream;
+    //SerialPort serialPort;
+    //Thread readThread;
+
+	
+	private static int dv;
+	static int re;
+	
+	
+	public static void main(String[] args) {
+		
+		
+		portList = CommPortIdentifier.getPortIdentifiers();
+     System.out.println(portList);
+     
+        while (portList.hasMoreElements()) {
+        	System.out.println("ff");
+            portId = (CommPortIdentifier) portList.nextElement();
+            System.out.println(portId.getName());
+            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                 if (portId.getName().equals("/dev/term/a")) {
+			//                if (portId.getName().equals("/dev/term/a")) {
+                    System.out.println("he"+portId.getName());
+                }
+            }
+        }
+        
+		
+		
+		dv=ThinkGear.GetDriverVersion();
+		re=ThinkGear.GetNewConnectionId();
+		System.out.println("he");
+		System.out.println(dv);
+		System.out.println("he");
+		System.out.println(re);
+		int hre=ThinkGear.BAUD_9600;
+		System.out.println(hre);
+		//String s= '\.';
+		int res=ThinkGear.Connect(re, "\\\\.\\COM13", hre, ThinkGear.STREAM_PACKETS);
+		System.out.println(res);
+		int dataset=ThinkGear.SetDataFormat(re, 0);
+		System.out.println(dataset);
+		double de= ThinkGear.GetValue(re, 3);
+		System.out.println(de);
+		int read=ThinkGear.ReadPackets(re, 1);
+		System.out.println(read);
+		
+		
+	}
+	/*static {
+        System.loadLibrary("thinkgear");
+    }*/
+	
+
+}
