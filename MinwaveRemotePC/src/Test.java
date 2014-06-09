5df29165dc8267a7e15bc2ab73aba05c32d25fef
Deowrat <Deowrat@Deowrat-PC.mshome.net>
2014-06-09 16:42:03 +0530

    deowrat vlc code uo[dated

--- MinwaveRemotePC/src/Test.java
+++ MinwaveRemotePC/src/Test.java
@@ -1,9 +1,123 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hwuert");
-	}
+	
+
+
+	
+		
+		private static BufferedReader bufin;
+		private static OutputStream out;
+
+		public static void main(String[] args) throws Exception{
+			
+			
+			InetAddress localhost = InetAddress.getByName("localhost");
+			
+		     Socket socket = new Socket(localhost, 2000);
+		     
+		     InputStream in = socket.getInputStream();
+		     
+
+	         bufin = new BufferedReader(new InputStreamReader(in));
+	         out = socket.getOutputStream();
+	         
+	         
+	         
+	         /*Thread inputThread = new Thread(new Runnable() {
+				
+				public void run() {
+					// TODO Auto-generated method stub
+					{
+			        	boolean ok = true;
+			            while(ok) {
+			              try {
+			                System.out.println(bufin.readLine());           
+			              }
+			              catch (Exception e) {
+			                System.out.println("Whoops: " + e.getMessage());
+			                ok = false;
+			              }
+			            }
+					}
+				}
+			});
+	         */
+	         
+	         Thread inputThread = new Thread(new Runnable() {
+				
+				public void run() {
+					// TODO Auto-generated method stub
+					boolean ok = true;
+		            while(ok) {
+		              try {
+		                System.out.println(bufin.readLine());           
+		              }
+		              catch (Exception e) {
+		                System.out.println("Whoops: " + e.getMessage());
+		                ok = false;
+		              }
+		            }
+				}
+
+			});
+	         inputThread.setDaemon(true);
+	         inputThread.start();
+	         
+	         Thread.sleep(200);
+	         out.write("help \n".getBytes());
+	         out.write("status \n".getBytes());
+	         //Thread.sleep(200);
+	         //out.write("volume 190 \n".getBytes());
+	         //out.write("pause \n".getBytes());
+	         Thread.sleep(200);
+	         out.close();
+	         in.close();
+	         socket.close();
+	         System.exit(0);
+			/*Thread inputThread = new Thread(new Runnable()
+	        {
+
+	        @Override
+	        public void run()
+	           {
+	        	boolean ok = true;
+	            while(ok) {
+	              try {
+	                System.out.println(bufin.readLine());           
+	              }
+	              catch (Exception e) {
+	                System.out.println("Whoops: " + e.getMessage());
+	                ok = false;
+	              }
+	            } 
+	           }
+	        });
+			inputThread.setDaemon(true);
+	     inputThread.start();
+	     
+	     Thread.sleep(200);
+	     out.write("help \n".getBytes());
+	     Thread.sleep(200);
+	     out.write("volume 190 \n".getBytes());
+	     out.write("pause \n".getBytes());
+	     Thread.sleep(200);
+	     out.close();
+	     in.close();
+	     socket.close();
+	     System.exit(0);
+			*/
+		}
+
+	
+
 
 }
+
