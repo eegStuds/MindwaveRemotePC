1d10119c2fb09d68773c7bd582d972198f648816
Deowrat <Deowrat@Deowrat-PC.mshome.net>
2014-06-13 21:07:39 +0530

    deowrat

--- MinwaveRemotePC/src/mygui/transparentframe.java
+++ MinwaveRemotePC/src/mygui/transparentframe.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
 
 public class transparentframe extends JFrame {
 
@@ -35,8 +36,26 @@ public class transparentframe extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel1 = new JPanel();
+		contentPane.add(panel1);
+		
+		JPanel panel2 = new JPanel();
+		contentPane.add(panel2);
+		
+		JPanel panel3 = new JPanel();
+		contentPane.add(panel3);
+		
+		JPanel panel4 = new JPanel();
+		contentPane.add(panel4);
+		
+		JPanel panel5 = new JPanel();
+		contentPane.add(panel5);
+		
+		JPanel panel6 = new JPanel();
+		contentPane.add(panel6);
 	}
 
 }
